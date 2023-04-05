6eafe5aa77b56f593516bcc34cb8912b474c47c1
Giancarlo <gvasquez2511@gmail.com>
2023-04-05 11:36:15 -0500

    2do ejercicio

---
+++ Javascript/02-Sintaxis/index.js
@@ -0,0 +1,11 @@
+const lista = ["Giancarlo Vasquez", 34, true,
+new Date(1988, 10, 25),
+{ 
+    titulo: "El código Da Vinci",
+    autor: "Dan Brown",
+    fecha: new Date(2003, 3, 1),
+    url: "https://www.amazon.com/-/es/Dan-Brown/dp/8408095331"
+    },
+];
+
+console.log(lista);
